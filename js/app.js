01a5b9e2d073c7b1b2e59cd2f65696c707b45091
Ron Richardson <ron.richardson@gmail.com>
2017-12-16 01:00:16 -0500

    new pages and functionality start

--- js/app.js
+++ js/app.js
@@ -1,52 +1,46 @@
 var cart = '';
 
 $(document).ready(function () {
-    changePage('home');
-
-    $('#login-button').click(function() {
-        changePage('swipe');
-    });
-
     $('.add-to-cart').click(function() {
         id = $(this).data('id');
         console.log(id);
         addToCart(id);
     });
-
-    function changePage(p) {
-        $('.page').hide();
-        $('#' + p + '-page').show();
-
-        if (p == 'swipe') {
-            initSwipe();
-        }
-    }
-
-    function initSwipe() {
-        var swiper = new Swiper('.swiper-container', {
-            effect: 'coverflow',
-            grabCursor: true,
-            centeredSlides: true,
-            slidesPerView: 'auto',
-            coverflowEffect: {
-                rotate: 50,
-                stretch: 0,
-                depth: 100,
-                modifier: 1,
-                slideShadows : true,
-            },
-            pagination: {
-                el: '.swiper-pagination',
-            },
-        });
-    
-        swiper.on('slideChange', function() {
-            //remove slide from list, depending on right/left
-            //console.log(swiper);
-        });
-    }
-
-    function addToCart(id) {
-
-    }
 });
+
+
+function initSwipe() {
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows : true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+
+    swiper.on('slideChange', function() {
+        //remove slide from list, depending on right/left
+        //console.log(swiper);
+    });
+}
+
+function addToCart(id) {
+
+}
+
+function loadFoodInfo(id) {
+    if (!id) {
+        id = getParameterByName('id');
+    }
+
+    console.log(id);
+}
