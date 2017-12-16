4860869e0f516fadd2da15b81c586b4b9a7d7fb8
Ron Richardson <ron.richardson@gmail.com>
2017-12-15 23:16:00 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false
+    });
+
+    function loadFoodSlides() {
+        
+    }
+
+    function updateFoodPreference() {
+
+    }
+});
