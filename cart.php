2b3474248a246ee990aff4bc70bb13f446c93a6c
Ron Richardson <ron.richardson@gmail.com>
2017-12-17 08:52:56 -0500

    Added mobile icons and manifests

--- cart.php
+++ cart.php
@@ -9,6 +9,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <?php include_once('head.php'); ?>
+
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
         <link rel="stylesheet" href="css/main.css" />
 
---
+++ head.php
@@ -0,0 +1,8 @@
+<link rel="apple-touch-icon" sizes="180x180" href="/img/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/icons/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="apple-mobile-web-app-title" content="Phoodie">
+<meta name="application-name" content="Phoodie">
+<meta name="theme-color" content="#ffffff">
